06d60ae3eecc90996e1473fed4e1d61b2d935e01
Nguyen <hoangnguyendepgiai@gmail.com>
2026-02-04 14:00:15 +0700

    finish register function

---
+++ src/main/java/com/handgrow/demo/dto/response/UserResponse.java
@@ -0,0 +1,23 @@
+package com.handgrow.demo.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private String id;
+    private String fullName;
+    private String username;
+    private String phoneNumber;
+    private String role;
+    private String avatarUrl;
+    private String address;
+    private String commune;
+    private String province;
+    private String produce;
+}
