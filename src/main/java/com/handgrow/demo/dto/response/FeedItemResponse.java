b83314e10d411ceffa097d1a509c3dc0f4b366e0
Nguyen <hoangnguyendepgiai@gmail.com>
2026-03-13 08:25:47 +0700

    HAN-18 update post

--- src/main/java/com/handgrow/demo/dto/response/FeedItemResponse.java
+++ src/main/java/com/handgrow/demo/dto/response/FeedItemResponse.java
@@ -1,7 +1,9 @@
 package com.handgrow.demo.dto.response;
 
 import com.handgrow.demo.entity.enums.FeedTargetType;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,6 +19,9 @@ public class FeedItemResponse {
     private FeedTargetType type;
     private String title;
     private String content;
+    private List<String> attachments;
+    private String productName;
+    private LocalDate expectedDate;
     private long likeCount;
     private long commentCount;
     private boolean isLiked;
